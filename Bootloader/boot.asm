a1f3d51d9ab8674719a2e3ab3255be9ef468a926
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-01-13 07:16:12 -0300

    Update boot.asm

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -7,6 +7,7 @@ BootMain:
         ;Setup Data
         cli
         cld
+
         xor     ax, ax
         mov     ds, ax
         mov     es, ax
@@ -14,9 +15,8 @@ BootMain:
         ;Setup Stack
         mov     sp, 0x7C00
         mov     ss, ax
-        sti
 
-        mov [bootdisk], dl
+        sti
 
         ;Setup VESA (640x480 8bpp)
         mov     ax, 0x4F02
@@ -31,25 +31,27 @@ BootMain:
 ;---------------------------------------
 
 ReadSectors:
-        mov cx, 10      ; counter
+        mov     cx, 10      ; counter
 
 .loop:
-        push cx
+        push    cx
 
-        mov ah, 0x42
-        mov dl, [bootdisk]
-        mov si, DAP
-        int 0x13
+        mov     ah, 0x42
+        mov     dl, [bootdisk]
+        mov     si, DAP
+        int     0x13
 
-        add dword [DAP+8], 127  ; increment sector count
-        add word [DAP+6], 0xFE0 ; (512*127) >> 4
+        add dword [DAP + 8], 127
+        add word  [DAP + 6], 0xFE0
 
         pop cx
+
         loop .loop
 
         ret
 
 align 4
+
 DAP:
         db      0x10        ;DAP     byte [dap+0]
         db      0x00        ;Unused  byte [dap+1]
@@ -57,8 +59,6 @@ DAP:
         dw      0x0000      ;Segment byte [dap+3] byte [dap+4]
         dw      KERNEL >> 4 ;Offset  word [DAP+6]
         dq      1           ;LBA      word [DAP+8]
-
-bootdisk:       db 0
 
 ;---------------------------------------
 
