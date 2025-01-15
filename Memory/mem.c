e16b2a4a8253a34bacf27c53cdd4fe152f5f3ca1
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-01-15 07:58:33 -0300

    Update mem.c
    added strcpy

--- Memory/mem.c
+++ Memory/mem.c
@@ -114,3 +114,18 @@ void* memcpy(void *dest, const void *src, DWORD n)
     return dest;
 }
 
+char* strcpy(char* dest, const char* src)
+{
+    char* original_dest = dest;
+
+    while (*src != '\0') 
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';
+
+    return original_dest;
+}
